c5892bae7c593778f2b92aaf5c550988dfafa0dc
hamza259599-cmd <hamza259599@gmail.com>
2026-09-10 18:07:48 +0500

    Fix SEO meta tags not updating: wrap App with HelmetProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HelmetProvider } from "react-helmet-async";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
@@ -6,6 +7,8 @@ import App from "@/App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+      <HelmetProvider>
     <App />
+      </HelmetProvider>
   </React.StrictMode>,
 );
